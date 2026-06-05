8f96f23b903da962fc6e4251a692dfa7d9c6e424
Gabrieli <gabrielidelimamartins@gmail.com>
2026-06-05 19:04:08 -0300

    exercicio 17

---
+++ exercicio17.js
@@ -0,0 +1,16 @@
+/*
+Dado o objeto pessoa com propriedades nome e idade, acesse e imprima o valor de
+idade. Adicione uma nova propriedade chamada email ao objeto pessoa que já possui
+nome e idade.
+*/
+
+let pessoa = {
+    nome : 'Ana',
+    idade : 18
+}
+
+console.log(pessoa.idade)
+
+pessoa.email = 'ana@email.com';
+
+console.log(pessoa)
